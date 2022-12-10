9fc8e0fb82583caff2f91c84fb9711f406b81188
Csendes Dávid <csendes.david@alerant.hu>
2022-12-10 11:40:02 +0100

    Added Actuator and logging

--- src/main/java/hu/webuni/cloud/api/controller/WebinarController.java
+++ src/main/java/hu/webuni/cloud/api/controller/WebinarController.java
@@ -3,6 +3,8 @@ package hu.webuni.cloud.api.controller;
 import hu.webuni.cloud.api.dto.WebinarRequest;
 import hu.webuni.cloud.api.dto.WebinarResponse;
 import hu.webuni.cloud.service.WebinarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +17,8 @@ import java.util.List;
 @RequestMapping("/webinar")
 public class WebinarController {
 
+    private static final Logger log = LoggerFactory.getLogger(WebinarController.class);
+
     private final WebinarService service;
 
     public WebinarController(WebinarService service) {
@@ -25,9 +29,13 @@ public class WebinarController {
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public WebinarResponse webinar(@RequestBody WebinarRequest request) {
+        log.info("Request arrived on /webinar endpoint with message: {}, leftNumber: {}, rightNumber: {}",
+                request.message(), request.leftNumber(), request.rightNumber());
         int result = service.calculate(request.leftNumber(), request.rightNumber());
         service.createNewMessage(request.message());
         List<String> recentMessages = service.retrieveRecentMessages();
+        log.info("Sending back response on /webinar endpoint with data: number of recentMessages {}, result: {}",
+                recentMessages.size(), result);
         return new WebinarResponse(recentMessages, result);
     }
 }
--- src/main/java/hu/webuni/cloud/service/WebinarService.java
+++ src/main/java/hu/webuni/cloud/service/WebinarService.java
@@ -1,12 +1,16 @@
 package hu.webuni.cloud.service;
 
 import hu.webuni.cloud.call.api.MessageApi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class WebinarService {
+
+    private static final Logger log = LoggerFactory.getLogger(WebinarService.class);
 
     private final MessageApi api;
 
@@ -15,14 +19,17 @@ public class WebinarService {
     }
 
     public int calculate(int leftNumber, int rightNumber) {
+        log.info("Calculating: {} + {}", leftNumber, rightNumber);
         return leftNumber + rightNumber;
     }
 
     public void createNewMessage(String message) {
+        log.info("Creating new message on API: {}", message);
         api.newMessageArrived(message);
     }
 
     public List<String> retrieveRecentMessages() {
+        log.info("Retrieving recent messages from API");
         return api.getRecentMessages();
     }
 }
